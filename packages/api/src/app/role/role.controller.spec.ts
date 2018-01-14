e59adcc7688fda1595947b7aecb8e0dc44c2e085
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2018-01-14 23:33:26 +0000

    More test stuff that doesnt work

--- packages/api/src/app/role/role.controller.spec.ts
+++ packages/api/src/app/role/role.controller.spec.ts
@@ -1,4 +1,28 @@
+import {Test} from '@nestjs/testing';
+import {TestingModule} from '@nestjs/testing/testing-module';
+import {RoleController} from './role.controller';
+import {RoleService} from './role.service';
+
 describe('Role controller', () => {
+  let controller: RoleController;
+  let service: RoleService;
+
+  beforeEach(async () => {
+    const mod: TestingModule = await Test
+      .createTestingModule({
+        components: [
+          RoleService
+        ],
+        controllers: [
+          RoleController
+        ]
+      })
+      .compile();
+
+    controller = mod.get<RoleController>(RoleController);
+    service = mod.get<RoleService>(RoleService);
+  });
+
   it('should be true', () => {
     expect(true).toBe(true);
   });
