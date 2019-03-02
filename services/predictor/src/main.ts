aa23d10bfa4528359f8c1991cffd642e4d5c57bf
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-03-02 17:36:31 +0000

    Sort out Sonar issues

--- services/predictor/src/main.ts
+++ services/predictor/src/main.ts
@@ -1,11 +1,10 @@
+// tslint:disable no-console
 /**
  * @author Mo Gusbi <me@mogusbi.co.uk>
  */
 import {bootstrap} from '@breezejs/bootstrap';
 import {AppModule} from './app';
 
-try {
-  void bootstrap(AppModule, 3000);
-} catch (e) {
-  throw e;
-}
+bootstrap(AppModule, 3000).catch((err: Error): void => {
+  console.error(err.message);
+});
